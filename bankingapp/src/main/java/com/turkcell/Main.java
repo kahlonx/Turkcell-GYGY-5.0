847caffbd9fe4e084ff3c145cbbc0f8b188cb90b
kahlonx <goncu.deniz2000@gmail.com>
2026-04-08 17:38:23 +0300

    feat: scenario 3 added

--- bankingapp/src/main/java/com/turkcell/Main.java
+++ bankingapp/src/main/java/com/turkcell/Main.java
@@ -6,10 +6,10 @@ public class Main {
         Account standartHesap = new Account("111", "Deniz", 500.0);
         SalaryAccount salaryHesap = new SalaryAccount("999", "Halil", 500.0);
 
-        System.out.println("---- Başlangıç Bakiyeleri ----");
+        System.out.println("\n---- Başlangıç Bakiyeleri ----");
         standartHesap.showBalance();
         salaryHesap.showBalance();
-        System.out.println("*---------------------------------*\n");
+        System.out.println("\n*---------------------------------*\n");
 
         System.out.println("Senaryo 1: Standart hesap para transferi yapıyor ( İşlem ücreti 5 TL.).");
         standartHesap.transfer(salaryHesap, 100.0);
@@ -21,5 +21,10 @@ public class Main {
         standartHesap.showBalance();
         salaryHesap.showBalance();
       
+        System.out.println("\nSenaryo 3: Para çekme ve para yatırma işlemleri.");
+        standartHesap.withdraw(50.0);
+        salaryHesap.deposit(200.0);
+        standartHesap.showBalance();
+        salaryHesap.showBalance();
     }
 }
