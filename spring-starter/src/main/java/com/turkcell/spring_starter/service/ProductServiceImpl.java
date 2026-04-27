5089d025d3d5cc7c1bb4e8e95a602b5530d302d1
kahlonx <goncu.deniz2000@gmail.com>
2026-04-27 16:02:59 +0300

    fix: unused imports removed from project

--- spring-starter/src/main/java/com/turkcell/spring_starter/service/ProductServiceImpl.java
+++ spring-starter/src/main/java/com/turkcell/spring_starter/service/ProductServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
