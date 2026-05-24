d2291d0daeb866b7c4968cf463d23fd1d7e2529d
kahlonx <goncu.deniz2000@gmail.com>
2026-05-24 17:45:10 +0300

    homework: ecommerce microservices architecture

---
+++ gygy5_microservices/product_service/src/main/java/com/turkcell/product_service/ProductServiceApplication.java
@@ -0,0 +1,25 @@
+package com.turkcell.product_service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ProductServiceApplication {
+
+	@Value("${spring.application.name}")
+	private String serviceName;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello " + serviceName;
+	}
+
+}
